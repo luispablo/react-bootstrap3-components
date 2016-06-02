cf02d2572d6d578dd896f45c70627b8874e65f37
Luis Pablo <lgallo@tsjbaires.gov.ar>
2016-06-02 12:44:35 -0300

    Export the new Select component

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var HorizontalFormCheckbox = require("./lib/HorizontalFormCheckbox");
 var HorizontalFormInput = require("./lib/HorizontalFormInput");
 var HorizontalFormRadioButton = require("./lib/HorizontalFormRadioButton");
+var Select = require("./lib/Select");
 
 var index = {
 	HorizontalFormCheckbox: HorizontalFormCheckbox,
 	HorizontalFormInput: HorizontalFormInput,
-	HorizontalFormRadioButton: HorizontalFormRadioButton
+	HorizontalFormRadioButton: HorizontalFormRadioButton,
+	Select: Select
 };
 
 module.exports = index;
--- test/index-test.js
+++ test/index-test.js
@@ -2,11 +2,13 @@ var test = require("tape");
 var HorizontalFormCheckbox = require("../lib/HorizontalFormCheckbox");
 var HorizontalFormInput = require("../lib/HorizontalFormInput");
 var HorizontalFormRadioButton = require("../lib/HorizontalFormRadioButton");
+var Select = require("../lib/Select");
 var index = require("../index");
 
 test("index - exports", function (assert) {
 	assert.equal(index.HorizontalFormCheckbox, HorizontalFormCheckbox, "require HorizontalFormCheckbox");
 	assert.equal(index.HorizontalFormInput, HorizontalFormInput, "require HorizontalFormInput");
 	assert.equal(index.HorizontalFormRadioButton, HorizontalFormRadioButton, "require HorizontalFormRadioButton");
+	assert.equal(index.Select, Select, "require Select");
 	assert.end();
 });
