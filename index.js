d1c5108ac89314b918c7aa1567a497ed68bc9337
Luis Pablo <lgallo@tsjbaires.gov.ar>
2016-06-03 12:16:21 -0300

    Add the new component to the exported set

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var HorizontalFormCheckbox = require("./lib/HorizontalFormCheckbox");
 var HorizontalFormInput = require("./lib/HorizontalFormInput");
+var HorizontalFormInputText = require("./lib/HorizontalFormInputText");
 var HorizontalFormRadioButton = require("./lib/HorizontalFormRadioButton");
 var Select = require("./lib/Select");
 
 var index = {
 	HorizontalFormCheckbox: HorizontalFormCheckbox,
 	HorizontalFormInput: HorizontalFormInput,
+	HorizontalFormInputText: HorizontalFormInputText,
 	HorizontalFormRadioButton: HorizontalFormRadioButton,
 	Select: Select
 };
--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,7 @@
 var test = require("tape");
 var HorizontalFormCheckbox = require("../lib/HorizontalFormCheckbox");
 var HorizontalFormInput = require("../lib/HorizontalFormInput");
+var HorizontalFormInputText = require("../lib/HorizontalFormInputText");
 var HorizontalFormRadioButton = require("../lib/HorizontalFormRadioButton");
 var Select = require("../lib/Select");
 var index = require("../index");
@@ -9,6 +10,7 @@ test("index - exports", function (assert) {
 	assert.equal(index.HorizontalFormCheckbox, HorizontalFormCheckbox, "require HorizontalFormCheckbox");
 	assert.equal(index.HorizontalFormInput, HorizontalFormInput, "require HorizontalFormInput");
 	assert.equal(index.HorizontalFormRadioButton, HorizontalFormRadioButton, "require HorizontalFormRadioButton");
+	assert.equal(index.HorizontalFormInputText, HorizontalFormInputText, "require HorizontalFormInputText");
 	assert.equal(index.Select, Select, "require Select");
 	assert.end();
 });
